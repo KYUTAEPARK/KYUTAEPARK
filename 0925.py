eb5e7dd8d76066e4c3f14ad89a2f5f892ff1c39c
KYUTAEPARK <acasia1214@naver.com>
2014-09-26 11:57:29 +0900

    0925.PY

--- 0925.py
+++ 0925.py
@@ -1,1 +1,19 @@
-ffff
+
+mylist = [ 45, 20, 21, 50, 10, 8, 9, 7, 10]
+def bubble(A):
+    length = len(A) - 1
+    unsorted = True
+
+    while unsorted:
+    	for i in range(0,length):
+		    unsorted = False
+		    if A[i] > A[i + 1]:
+			    hold = A[i + 1]
+			    A[i + 1] = A[i]
+			    A[i] = hold
+			    print A
+		    else:
+			    unsorted = True
+print bubble(mylist)
+
+
